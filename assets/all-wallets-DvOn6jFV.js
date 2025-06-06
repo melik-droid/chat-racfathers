70a4b6d41d638fb2bfc1054317c3483abf136cc9
melik-droid <melikcaymaz@gmail.com>
2025-06-06 14:09:18 +0300

    Updates

---
+++ assets/all-wallets-DvOn6jFV.js
@@ -0,0 +1,6 @@
+import{b as o}from"./core-CRVJyCOH.js";import"./index-pn5jrqFy.js";import"./index.es-Hun9yDqf.js";import"./events-DQ172AOg.js";const r=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{r as allWalletsSvg};
