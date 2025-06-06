a847df66eecb8f797516b09b80ac2f35698b1dd4
melik-droid <melikcaymaz@gmail.com>
2025-06-06 14:19:40 +0300

    Updates

--- assets/close-C3unAd-i.js
+++
@@ -1,8 +0,0 @@
-import{b as l}from"./core-CRVJyCOH.js";import"./index-pn5jrqFy.js";import"./index.es-Hun9yDqf.js";import"./events-DQ172AOg.js";const t=l`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M2.54 2.54a1 1 0 0 1 1.42 0L8 6.6l4.04-4.05a1 1 0 1 1 1.42 1.42L9.4 8l4.05 4.04a1 1 0 0 1-1.42 1.42L8 9.4l-4.04 4.05a1 1 0 0 1-1.42-1.42L6.6 8 2.54 3.96a1 1 0 0 1 0-1.42Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as closeSvg};
