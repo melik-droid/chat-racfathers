2db8b0bb0d080f4fa808401fd96333598004c9a7
melik-droid <melikcaymaz@gmail.com>
2025-06-10 18:36:45 +0300

    Updates

---
+++ assets/chevron-top-C77nfTea.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-C1_SMgny.js";import"./index-D_yyG_tr.js";import"./index.es-Bjlv6BdQ.js";import"./events-DQ172AOg.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as chevronTopSvg};
