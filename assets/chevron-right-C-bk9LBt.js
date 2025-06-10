fee55488e38cf8d91efa492000df501f7eeccaa2
melik-droid <melikcaymaz@gmail.com>
2025-06-10 19:10:08 +0300

    Updates

--- assets/chevron-right-C-bk9LBt.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-C1_SMgny.js";import"./index-D_yyG_tr.js";import"./index.es-Bjlv6BdQ.js";import"./events-DQ172AOg.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronRightSvg};
