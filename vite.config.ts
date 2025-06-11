701e7fab795cf967a45474c9f6ea6356ff57ef93
Tuguberk <akbulut.tugberk@gmail.com>
2025-06-12 01:16:06 +0300

    changed base url for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chat-racfathers/', 
+  base: '/', // /chat-racfathers
   plugins: [
     react(),
     tailwindcss(),
